910216934edcb8b79cf899af07cff8fdf4379c56
Aleksey Denisov <byslove.97@gmail.com>
2025-06-25 08:25:54 +0400

    Добавил форму логина и стили

---
+++ src/features/auth/components/LoginForm.tsx
@@ -0,0 +1,51 @@
+import styles from './LoginForm.module.scss';
+import { Form, Input, Button } from 'antd';
+import type { FormProps } from 'antd';
+import { Link } from 'react-router-dom';
+
+type LoginFormFields = {
+  email: string;
+  password: string;
+};
+
+const LoginForm: React.FC = () => {
+  const onFinish: FormProps<LoginFormFields>['onFinish'] = (values) => {
+    console.log('Login values:', values);
+  };
+
+  return (
+    <div className={styles.formWrapper}>
+      <div className={styles.title}>Вход</div>
+      <Form<LoginFormFields> name="login" layout="vertical" onFinish={onFinish}>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            { required: true, message: 'Введите email' },
+            { type: 'email', message: 'Некорректный email' },
+          ]}
+        >
+          <Input placeholder="Введите почту" />
+        </Form.Item>
+
+        <Form.Item
+          label="Пароль"
+          name="password"
+          rules={[{ required: true, message: 'Введите пароль' }]}
+        >
+          <Input.Password placeholder="••••••••" />
+        </Form.Item>
+        <Form.Item className={styles.registerLink}>
+          Еще нет аккаунта? <Link to="/register">Зарегистрируйтесь</Link>
+        </Form.Item>
+        <Form.Item className={styles.centeredButton}>
+  <Button className={styles.centeredButton} type="primary" htmlType="submit">
+    Войти
+  </Button>
+</Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default LoginForm;
