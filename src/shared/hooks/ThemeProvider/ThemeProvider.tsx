646666b6a853f53c9dedef9fc008d175885bd1a0
greyskin <hissra6@gmail.com>
2025-08-03 18:59:38 +0300

    AV-282 Theme switch

---
+++ src/shared/hooks/ThemeProvider/ThemeProvider.tsx
@@ -0,0 +1,20 @@
+import { ThemeContext } from './ThemeContext';
+import React, { useState, useEffect } from 'react';
+
+export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [theme, setTheme] = useState<'light' | 'dark'>(() => {
+    const savedTheme = localStorage.getItem('theme');
+    return (savedTheme as 'light' | 'dark') || 'light';
+  });
+
+  useEffect(() => {
+    document.body.className = `theme-${theme}`;
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
+};
--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,9 +1,12 @@
 import styles from './Header.module.scss';
+import { useTheme } from '@/shared/hooks/ThemeProvider/useTheme';
 import logo from '@assets/icons/logo.svg';
-import { Button } from 'antd';
+import { Button, Switch } from 'antd';
+import { FaSun, FaMoon } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
     <header className={styles.header}>
       <div className={styles.inner}>
@@ -16,6 +19,13 @@ const Header: React.FC = () => {
             <Button type="default">Вход</Button>
           </Link>
           <Button type="default">Регистрация</Button>
+          <Switch
+            checkedChildren={<FaSun style={{ marginTop: 4, height: 15, width: 15 }} />}
+            unCheckedChildren={<FaMoon style={{ marginBottom: 2 }} />}
+            defaultChecked
+            checked={theme === 'light'}
+            onChange={toggleTheme}
+          />
         </div>
       </div>
     </header>
