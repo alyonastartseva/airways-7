bc4f723f323e10a3b7ca462f159584fc7952c209
arlekin13 <arlekin13rus@gmail.com>
2025-08-07 13:03:20 +0300

    отредактировал хедер, логика отображения кнопок при авторизации,оставил поиск билетов на главной

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,12 +1,22 @@
 import styles from './Header.module.scss';
 import { useTheme } from '@/shared/hooks/ThemeProvider/useTheme';
+import { useAuthToken } from '@/shared/hooks/useAuthToken';
 import logo from '@assets/icons/logo.svg';
 import { Button, Switch } from 'antd';
+import React from 'react';
 import { FaSun, FaMoon } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Header: React.FC = () => {
   const { theme, toggleTheme } = useTheme();
+  const { user, isAuthenticated, removeToken } = useAuthToken();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    removeToken();
+    navigate('/');
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.inner}>
@@ -14,11 +24,35 @@ const Header: React.FC = () => {
           <img src={logo} alt="Air Alien" className={styles.logoIcon} />
           <span className={styles.logoText}>Air Alien</span>
         </Link>
+        <nav className={styles.nav}>
+          <Link to="/admin/passengers">Пассажиры</Link>
+          <Link to="/admin/flights">Самолеты</Link>
+          <Link to="/admin/tickets">Места</Link>
+          <Link to="/">Пояса</Link>
+          <Link to="/">Рейсы</Link>
+          <Link to="/">Билеты</Link>
+          <Link to="/">Бронирование</Link>
+          <Link to="/">Посадочные места</Link>
+          <Link to="/admin/passengers-table">Пользователи</Link>
+        </nav>
         <div className={styles.authButtons}>
-          <Link to="/login">
-            <Button type="default">Вход</Button>
-          </Link>
-          <Button type="default">Регистрация</Button>
+          {isAuthenticated ? (
+            <>
+              <span>{user?.username}</span>
+              <Button type="default" onClick={handleLogout}>
+                Выйти
+              </Button>
+            </>
+          ) : (
+            <>
+              <Link to="/login">
+                <Button type="default">Вход</Button>
+              </Link>
+              <Button type="default" onClick={() => navigate('/register')}>
+                Регистрация
+              </Button>
+            </>
+          )}
           <Switch
             checkedChildren={<FaSun style={{ marginTop: 4, height: 15, width: 15 }} />}
             unCheckedChildren={<FaMoon style={{ marginBottom: 2 }} />}
