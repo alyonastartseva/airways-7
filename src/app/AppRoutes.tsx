ad5031c330d1bfeef44861a40e81f71054a0a651
Danil Buntovskikh <buntovskikhds@gmail.com>
2025-08-12 02:30:09 +0300

    убрал комментарий

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -1,6 +1,5 @@
 import LoginPage from '@/pages/Auth/LoginPage';
 import AdminPassengerPage from '@/pages/admin/passengers';
-// import PassengersTable from '@entities/PassengersTable';
 import HomePage from '@pages/home/HomePage';
 import Layout from '@widgets/layout/Layout';
 import { Routes, Route } from 'react-router-dom';
