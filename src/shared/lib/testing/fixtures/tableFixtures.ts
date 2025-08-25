52cd8ba2f180afb2a1f8ac5cc444e759dc410020
AssasinDance <159630826+AssasinDance@users.noreply.github.com>
2025-08-25 17:19:12 +0300

    свел верстку таблицы пассажиров с макетом (#24)
    * свел верстку теблицы пассажиров с макетом
    
    * починил кнопку и перенес инлайн стили
    
    * починил деплой
    
    * внес правки

--- src/shared/lib/testing/fixtures/tableFixtures.ts
+++ src/shared/lib/testing/fixtures/tableFixtures.ts
@@ -10,6 +10,7 @@ export interface defaultPropsType<T> {
   isError?: boolean;
   rowKey: keyof T;
   onError?: () => void;
+  children?: React.ReactNode;
 }
 
 export const defaultProps: defaultPropsType<MockPassengerType> = {
